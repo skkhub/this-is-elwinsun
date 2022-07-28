fd99f731907476f3edce2d134f56d1e2cf7e0a10
elwinsun <291742496@qq.com>
2022-07-29 00:18:42 +0800

    youhua

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,6 @@ import '../styles/index.scss';
 
 export default function MyApp({ Component, pageProps }) {
     return (
-      <>
-      <head>
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </head>
         <Component {...pageProps} />
-      </>
       )
   }
