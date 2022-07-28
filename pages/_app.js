105ff740db642af46ee38dac5901bdd529cb0616
elwinsun <291742496@qq.com>
2022-07-28 21:14:28 +0800

    favicon3

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/index.scss';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+      <>
+      <head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
+        <Component {...pageProps} />
+      </>
+      )
   }
